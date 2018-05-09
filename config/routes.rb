767490b51ba2f7265e2618467d07090309dd9e4e
VeronikaDoschenko <veronikad988@yandex.ru>
2018-05-10 01:39:50 +0300

    Com5

--- config/routes.rb
+++ config/routes.rb
@@ -14,15 +14,18 @@ Rails.application.routes.draw do
 
   resources :criteria
 
+  post 'criteria/:id' => 'criteria#update'
+
   get 'criteria/:id/edit/crit_scale_new' => 'criteria#crit_scale_new', as: "criteria_crit_scale"
   post 'criteria/:id/edit/crit_scale_new' => 'criteria#crit_scale_new'
 
   post 'criteria/:id/edit/crit_scale_create' => 'criteria#crit_scale_create'
 
-  post 'criteria/num_of_task' => 'criteria#num_of_task'
-
   post 'criteria/:id/edit/crit_scale_destroy' => 'criteria#crit_scale_destroy', as: "criteria_crit_scale_destroy"
   get 'criteria/:id/edit/crit_scale_destroy' => 'criteria#crit_scale_destroy'
+
+
+  post 'criteria/num_of_task' => 'criteria#num_of_task'
 
 
 end
